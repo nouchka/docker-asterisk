b6fc68be28accf0e3776482f1961431f48c604be
nouchka <github@katagena.com>
2017-02-20 18:04:04 +0000

    chown only conf and sh files

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp /etc/asterisk/conf/* /etc/asterisk/
-chown -R asterisk: /etc/asterisk/
+chown -R asterisk: /etc/asterisk/*.conf
+chown -R asterisk: /etc/asterisk/*.sh
 chmod +x /etc/asterisk/*.sh
 
 /usr/sbin/asterisk -f -U asterisk -G asterisk -vvvg -c
